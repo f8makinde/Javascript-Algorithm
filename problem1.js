45185de2ce2a9c9c94a277622e7be382a73b81c7
Faith Makinde <faithmakinde0@gmail.com>
2022-08-05 12:45:40 +0100

    Calculating area and perimeter of a rectangle

---
+++ problem1.js
@@ -0,0 +1,9 @@
+function areaOfARectangle(length, width){
+  return length * width;
+}
+console.log(areaOfARectangle(2, 4));
+
+function perimeterOfARectangle(length, width){
+    return 2 * (length * width);
+}
+console.log(perimeterOfARectangle(4, 6));
