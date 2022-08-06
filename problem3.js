2008e6eeb68e254e997a515bcb7b0c80c9ab9e1c
Faith Makinde <faithmakinde0@gmail.com>
2022-08-06 16:09:43 +0100

    work-completed

--- problem3.js
+++ problem3.js
@@ -2,6 +2,11 @@ function quadraticEquation(a, b, c){
     let numeratorOne = -b + (Math.sqrt(b**2 -4*a*c));
     let numeratorTwo = -b - (Math.sqrt(b**2 -4*a*c));
     let denominator = 2*a;
-    return "x = " + numeratorOne/denominator + " and x = " + numeratorTwo/denominator;
+    if(a === 0 || b === 0 || c === 0){
+        return `This is not a quardratic equation`
+    }
+    else{
+        return `x = ${numeratorOne/denominator} and x = ${numeratorTwo/denominator}`;
+    }
 }
 console.log(quadraticEquation(2, 9, 9));
