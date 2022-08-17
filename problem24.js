a345fbf76fdb7954a8ce7fe46eba0db57bd85b7a
Faith Makinde <faithmakinde0@gmail.com>
2022-08-17 23:23:47 +0100

    multiply all numbers using array method

---
+++ problem24.js
@@ -0,0 +1,10 @@
+function multiplyAllNumbers(array){
+    let product = 1;
+    array.forEach(element => {
+        element.forEach(el => {
+          product *= el;
+        })
+    })
+    return product;
+}
+console.log(multiplyAllNumbers([[1, 2, 3, 4]]))
