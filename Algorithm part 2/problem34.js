6179470feba5f007111c6688c4d4c075e4393725
Faith Makinde <faithmakinde0@gmail.com>
2022-09-16 01:12:01 +0100

    Linear search

--- Algorithm part 2/problem34.js
+++ Algorithm part 2/problem34.js
@@ -1,10 +1,10 @@
 function present(arr, num){
     for(let i = 0; i < arr.length; i++){
         if(arr[i] === num){
-            return i
+            return i;
         }
     }
-    return i
+    return -1;
 }
 
-console.log(present([5, 4, 3, 2, 1], 1));
+console.log(present([5, 4, 3, 2, 1], 8));
