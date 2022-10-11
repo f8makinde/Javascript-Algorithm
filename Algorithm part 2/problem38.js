379f537c1fc35e1869d906feea093f4eee14986b
Faith Makinde <faithmakinde0@gmail.com>
2022-10-11 02:22:11 +0100

    demerit points

---
+++ Algorithm part 2/problem38.js
@@ -0,0 +1,17 @@
+function checkSpeed(speed){
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+  if(speed < speedLimit + kmPerPoint){
+    return "Ok";
+  }
+  else{
+   const points = Math.floor((speed - speedLimit) / kmPerPoint);
+   if(points >= 12){
+    return "License suspended";
+   }
+   else{
+    return `Points: ${points}`;
+   }
+  }
+}
+console.log(checkSpeed(130))
