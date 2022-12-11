0bd544901d4b612aee28416b0f893a0ad919620b
Faith Makinde <faithmakinde0@gmail.com>
2022-12-11 23:43:20 +0100

    praticing es6 single line code with array method

---
+++ Algorithm part 2/problem47.js
@@ -0,0 +1,9 @@
+const voters = [
+    {name: "Joe", email: "joe@joe.com", voted: true},
+    {name: "Jane", email: "jane@jane.com", voted: true},
+    {name: "Bo", email: "bo@bo.com", voted: false},
+    {name: "Bane", email: "bane@bane.com", voted: false}
+]
+
+const result = voters.filter(voters => voters.voted).map(voters => voters.email)
+console.log(result)
