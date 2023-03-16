9472a5a666e92f0dd5fc31ad6644fff67ef79ff8
Faith Makinde <faithmakinde0@gmail.com>
2023-03-16 09:38:10 +0100

    binary search

--- Algorithm part 2/problem51.js
+++ Algorithm part 2/problem51.js
@@ -1,10 +1,27 @@
-function recursiveFactorial(n){
-    if(n === 0){
+// function recursiveFactorial(n){
+//     if(n === 0){
+//         return 1
+//     }
+//     return n * recursiveFactorial(n - 1)
+// }
+// console.log(recursiveFactorial(0))
+// console.log(recursiveFactorial(4))
+// console.log(recursiveFactorial(5))
+// console.log(recursiveFactorial(1))
+function log(num){
+    if(num > 5){
+        return;
+    }
+    console.log(num);
+    log(num + 1);
+}
+
+log(1);
+
+function factorial (n){
+    if( n === 0){
         return 1
     }
-    return n * recursiveFactorial(n - 1)
+    return n * factorial(n -1)
 }
-console.log(recursiveFactorial(0))
-console.log(recursiveFactorial(4))
-console.log(recursiveFactorial(5))
-console.log(recursiveFactorial(1))
+console.log(factorial(1))
